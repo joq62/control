2f55c9a73c365585bcd688c8de1fe21a5bfb71f9
joq62 <jakim.leche@gmail.com>
2023-12-20 10:42:17 +0100

    added install and test script

--- raspi_scripts/test_control.sh
+++ raspi_scripts/test_control.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-sudo rm -rf control;
-sudo rm -rf logs;
-git clone https://github.com/joq62/control.git;
 erl -pa control/ebin -sname control_a  -setcookie a -run control start;
 echo Eagle has wings
