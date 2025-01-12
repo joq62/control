45cad7b43bcd64b59a9b4065652ca89e7ece423e
joq62 <joq62@c50>
2025-01-12 22:12:40 +0100

    corr

--- build_start_control.sh
+++ build_start_control.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# test script  
+# build and start script  
 
-# path_to_release_exec=$1
-# mode=$2, mode daemon | foreground
 
 
 rm -rf erl_cra* rebar3_crashreport;
 rm -rf *~ */*~ */*/*~ */*/*/*~;
 rm -rf control
-git clone 
-cd $1
+git clone https://github.com/joq62/control.git
+cd control
 rm -rf _build
 rebar3 release
 ./_build/default/rel/control/bin/control daemon
