4b2f6d8586245e6001a17c9d4253a916807275ab
joq62 <joq62@c50>
2025-01-14 22:30:30 +0100

    fix rebar3 path in appl_server

--- build_start_control.sh
+++ build_start_control.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # build and start script  
-
-
-
-rm -rf erl_cra* rebar3_crashreport;
-rm -rf *~ */*~ */*/*~ */*/*/*~;
-rm -rf control
-git clone https://github.com/joq62/control.git
-cd control
-rm -rf _build
-rebar3 release
-./_build/default/rel/control/bin/control daemon
-#./_build/default/rel/control/bin/control foreground
+#rm -rf erl_cra* rebar3_crashreport;
+#rm -rf *~ */*~ */*/*~ */*/*/*~;
+#rm -rf control
+#git clone https://github.com/joq62/control.git
+#cd control
+#rm -rf _build
+#rebar3 release
+cd /home/ubuntu
+./control/_build/default/rel/control/bin/control daemon &
